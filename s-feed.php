4839f2b197fe86ebe63d7cb4b421aed4fe2cd441
Marco Pelloni <mgpelloni@gmail.com>
2022-07-05 10:41:52 -0700

    Esuna Attribution + Updates

--- s-feed.php
+++ s-feed.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: S-Feed
-Plugin URI:  https://raindrop.agency/plugins/
+Plugin URI:  https://esuna.dev/plugins/s-feed/
 Description: Fetches simple Instagram data using the Basic Display API.
-Version:     1.0.1
-Author:      Raindrop Agency
-Author URI:  https://www.marcopelloni.com/
+Version:     1.0.2
+Author:      Esuna
+Author URI:  https://esuna.dev/
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
@@ -28,8 +28,8 @@ require_once(SFEED_PLUGIN_PATH . 'src/php/users.php');
 // Updates
 require 'includes/plugin-update-checker-4.4/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://social-feed.tech/plugins/sfeed/update.json',
+	'https://esuna.dev/ext/s-feed/update.json',
 	__FILE__, //Full path to the main plugin file or functions.php.
-	'sfeed'
+	's-feed'
 );
 ?>
